538e2c4b3450bb42f9e9a1de47e3eb8177e661ad
Clement Matheron <clement.matheron@gmail.com>
2015-05-15 22:51:10 +0200

    first problem done

---
+++ caesar-cipher.rb
@@ -0,0 +1,30 @@
+def caesar_cipher text, key	
+	result = []
+	text.split("").each do |c|
+		c = c.ord
+		if c >64 && c<91
+			c+=key
+			if c>91
+				c-=26
+			end
+		elsif c >96 && c<123
+			c+=key
+			if c>123
+				c-=26
+			end
+		else
+			c.chr
+		end
+		result << c.chr
+	end
+	result.join("")
+end
+
+def better_caesar text, key
+	alphabet = ('a'..'z').to_a.join
+	permutation = alphabet[key..-1] + alphabet[0...key]
+	text.tr!(alphabet, permutation)
+	text.tr(alphabet.upcase, permutation.upcase)
+end
+
+puts better_caesar("What a String!", 5)
